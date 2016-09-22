26e14b06a515cf1a9c09a8a39a7da28ba102cbec
Paul Ragar <pragar@Pauls-MacBook-Pro.local>
2016-09-22 15:02:33 -0600

    Completed

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,15 +4,23 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
-
+function isTyler(name) {
+  if (name === 'Tyler') {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 //////////////////PROBLEM 2////////////////////
 
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+function getName() {
+  return prompt("What is your name?");
+}
 
 
 //////////////////PROBLEM 3////////////////////
@@ -22,7 +30,9 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+function welcome() {
+  alert("Welcome, " + getName());
+}
 
 
 //////////////////PROBLEM 4////////////////////
@@ -42,7 +52,7 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+
 
 
 
@@ -52,17 +62,19 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
-  
+function myName() {
+  return "Paul";
+}
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName;
 
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName());
 
 //////////////////PROBLEM 7////////////////////
 
@@ -70,10 +82,12 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+function outerFn() {
+  return newMyName;
+}
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+var innerFn = outerFn();
 
 //Now invoke innerFn.
